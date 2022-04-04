ab9fd2c6556a23cfb6f73c18e83b0b98ad2c7df2
volbot <lucasmcdermitt@icloud.com>
2022-04-04 00:17:02 -0400

    basic game files

---
+++ src/floor.rs
@@ -0,0 +1,23 @@
+//Maquinita Floor (the map)
+// len + wid are length and width
+// states is an array of what is in each area
+pub struct MaqFloor {
+    len: usize,
+    wid: usize,
+    states: Box<[u8]>,
+}
+
+pub fn init_floor() -> MaqFloor {
+    //create the floor size
+    const L: usize = 10;
+    const W: usize = 10;
+    //allocate map data
+    let states = [0;L*W];
+
+    let floor = MaqFloor {
+        len: L,
+        wid: W,
+        states: Box::new(states)
+    };
+    floor
+}
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use macroquad::prelude::*;
+
+pub mod floor;
+
+#[macroquad::main("BasicShapes")]
+async fn main() {
+
+    init_game().await;
+
+    loop {
+        clear_background(RED);
+
+        if()
+
+        next_frame().await
+    }
+}
+
+async fn init_game() {
+    
+}
